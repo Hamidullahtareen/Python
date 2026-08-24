c4a70e84800be09b9f680eac9d2a86a7aecddc1b
hamidullahtareen <hamidullah.tareen@metropolia.fi>
2026-08-24 22:49:14 +0300

    edited mod01

--- Mod01/Hello.py
+++ Mod01/Hello.py
@@ -1,3 +1,4 @@
+"""
 print("Git-demo")
 
 print("Tervetuloa ohjelmaan!")
@@ -5,3 +6,7 @@ print("Tervetuloa ohjelmaan!")
 name = input("Anna nimesi: ")
 
 print("Hei", name)
+"""
+
+name = input("What is your name?")
+print(f"Hei {name}!")
