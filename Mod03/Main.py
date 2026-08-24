82f5b5788edac3eb96a6f11ad759bf94dfde1324
hamidullahtareen <hamidullah.tareen@metropolia.fi>
2026-08-24 22:28:31 +0300

    Tehtävä 3 valmis

--- Mod03/Main.py
+++ Mod03/Main.py
@@ -1,4 +1,5 @@
 
+
 # Tehtävä 3.1
 name = input ("Mikä on sinun nimesi?: ")
 print ("Hei " + name + "! Mukava tavata sinut.")
@@ -43,3 +44,29 @@ print("Sinun antama numeron tulo on:" , tulon)
 
 keskiarov = summa / 2
 print("Sinun antama numeron keskiarvo on:" , keskiarov)
+
+#Tehtävä 3.5
+question_levi = float(input("kuinka paljon painoa leiviskät on?"))
+question_naulat = float(input("kuinka paljon painoa naulat on?"))
+question_luodit = float(input("kuinka paljon painoa luodit on?"))
+
+levi = question_levi * 8.512
+naulat = question_naulat * 0.4256
+luodit = question_luodit * 0.0133
+
+summa = levi + naulat + luodit
+gramma = summa / 1000
+print(f"Massa nykymuttojen mukaan: {summa:.1f} Killogramma ja {gramma:.4f} gramma")
+
+
+#Tehtävä 3.6
+import random
+
+luku = random.randint(0,9)
+luku2 = random.randint(0,9)
+luku3 = random.randint(0,9)
+
+print(f"{random.randint(0,9)} {random.randint(0,9)} {random.randint(0,9)}")
+
+print(f"{random.randint(0,6)} {random.randint(0,6)} {random.randint(0,6)} {random.randint(0,6)}")
+
