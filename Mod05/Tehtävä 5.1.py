b0e3648e376137661b3455078de8819ceb1de1a2
hamidullahtareen <hamidullah.tareen@metropolia.fi>
2026-08-29 19:04:05 +0300

    Tehtävä 5 aloitus

---
+++ Mod05/Tehtävä 5.1.py
@@ -0,0 +1,6 @@
+n = 1000
+i = 1
+while  i <= n:
+    if i % 2 == 0:
+        print(f"{i} is even")
+    i += 1
---
+++ Mod05/Tehtävä 5.2.py
@@ -0,0 +1,5 @@
+inch = float(input("Enter number by inch: "))
+while inch > 0:
+    print(f"{inch}inch is {inch * 2.54}cm")
+    inch = float(input("Enter number by inch: "))
+print("Invalid number")
